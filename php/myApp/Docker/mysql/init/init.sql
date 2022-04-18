408889693aba4865be51e6b5f50cd4895e1c1471
hiramiya523 <hiramiya523@gmial.com>
2022-04-19 02:34:36 +0900

    環境変更

--- php/myApp/Docker/mysql/init/init.sql
+++ php/myApp/Docker/mysql/init/init.sql
@@ -1,6 +1,6 @@
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS first_db.users;
 
-CREATE TABLE users (
+CREATE TABLE first_db.users (
   id int(10) unsigned not null auto_increment,
   name varchar(255) not null,
   primary key (id)
